656aa1c28237e60f0b27f0b9f2e48ae7e72a06f6
youhei <youhei@jsk.imi.i.u-tokyo.ac.jp>
2012-11-27 07:36:25 +0000

    update android_sensor_message

--- jsk_android_apps/android_sensor_message/src/org/ros/android/androidSensorMessage/AndroidSensorService.java
+++ jsk_android_apps/android_sensor_message/src/org/ros/android/androidSensorMessage/AndroidSensorService.java
@@ -23,156 +23,163 @@ import android.os.Bundle;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.TextView;
 
-public class AndroidSensorService extends IntentService{
+public class AndroidSensorService extends IntentService {
 	SensorManager mSensorManager;
 	List<Sensor> sensors;
 	AndroidSensorMessageNode main_node;
-	NodeMain talker;
 	Intent mintent;
 	int[] usedFlag;
 	int[] usedFlag2;
 	final IBinder binder = new MyBinder();
-	
-	public class MyBinder extends Binder{
-		AndroidSensorService getService(){
+
+	public class MyBinder extends Binder {
+		AndroidSensorService getService() {
 			return AndroidSensorService.this;
 		}
 	}
-	
-	
-	public AndroidSensorService(){
+
+	public AndroidSensorService() {
 		super("Service");
 	}
-	
+
 	@Override
-	public IBinder onBind(Intent intent){
+	public IBinder onBind(Intent intent) {
 		mintent = intent;
-		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-		
+
 		usedFlag = new int[9];
 		usedFlag2 = new int[9];
-		for(int i=0;i<9;i++){
-			usedFlag[i]=0;
-			usedFlag2[i]=0;
+		for (int i = 0; i < 9; i++) {
+			usedFlag[i] = -1;
+			usedFlag2[i] = -1;
 		}
-		
-		
-		for(Sensor sensor: sensors){
-		    int sensorType = sensor.getType();
-		    switch(sensorType){
-			/*case Sensor.TYPE_AMBIENT_TEMPERATURE:
-			 this.sensorManager.registerListener(this.sensorListener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
-			   break;*/
-		    case Sensor.TYPE_ACCELEROMETER:
-		    	usedFlag[0]=1;
-			break;
-		    case Sensor.TYPE_TEMPERATURE:
-		    	usedFlag[1]=1;
-			break;
-		    case Sensor.TYPE_MAGNETIC_FIELD:
-		    	usedFlag[2]=1;
-			break;
-		    case Sensor.TYPE_LIGHT:
-		    	usedFlag[3]=1;
-			break;
-		    case Sensor.TYPE_PROXIMITY:
-		    	usedFlag[4]=1;
-			break;
-		    case Sensor.TYPE_GYROSCOPE:
-		    	usedFlag[5]=1;
-			break;
-		    case Sensor.TYPE_PRESSURE:
-		    	usedFlag[6]=1;
-	    		break;
-		    case Sensor.TYPE_GRAVITY:
-		    	usedFlag[7]=1;
-	        	break;
-		    case Sensor.TYPE_ROTATION_VECTOR:
-		    	usedFlag[8]=1;
-			break;
-			 /*   case Sensor.TYPE_RELATIVE_HUMIDITY:
-			 this.sensorManager.registerListener(this.sensorListener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
-			    break;*/
-		   }
+
+		for (Sensor sensor : sensors) {
+			int sensorType = sensor.getType();
+			switch (sensorType) {
+			/*
+			 * case Sensor.TYPE_AMBIENT_TEMPERATURE:
+			 * this.sensorManager.registerListener(this.sensorListener, sensor,
+			 * SensorManager.SENSOR_DELAY_FASTEST); break;
+			 */
+			case Sensor.TYPE_ACCELEROMETER:
+				usedFlag[0] = 1;
+				break;
+			case Sensor.TYPE_TEMPERATURE:
+				usedFlag[1] = 1;
+				break;
+			case Sensor.TYPE_MAGNETIC_FIELD:
+				usedFlag[2] = 1;
+				break;
+			case Sensor.TYPE_LIGHT:
+				usedFlag[3] = 1;
+				break;
+			case Sensor.TYPE_PROXIMITY:
+				usedFlag[4] = 1;
+				break;
+			case Sensor.TYPE_GYROSCOPE:
+				usedFlag[5] = 1;
+				break;
+			case Sensor.TYPE_PRESSURE:
+				usedFlag[6] = 1;
+				break;
+			case Sensor.TYPE_GRAVITY:
+				usedFlag[7] = 1;
+				break;
+			case Sensor.TYPE_ROTATION_VECTOR:
+				usedFlag[8] = 1;
+				break;
+			/*
+			 * case Sensor.TYPE_RELATIVE_HUMIDITY:
+			 * this.sensorManager.registerListener(this.sensorListener, sensor,
+			 * SensorManager.SENSOR_DELAY_FASTEST); break;
+			 */
+			}
 
 		}
 
 		return binder;
 	}
-		
-	public int getSensor(int i){
+
+	public int getSensor(int i) {
 		return usedFlag[i];
 	}
 	
-	
-	
-		public void setNode(int[] flag){
-		  showNotification();
-		  
-			for(int i=0;i<9;i++){
-				usedFlag2[i]=flag[i];
-				Log.v("test",i+":"+usedFlag2[i]);
-			}
-		    
-		    // We use this bundle
-		    Bundle b = mintent.getExtras();
-		    
-		    
-		    if(this.main_node == null){
-		    	try{
-		    	URI masterUri = new URI(b.getString("masterUri"));
-			  String hostLocal = InetAddressFactory.newNonLoopback().getHostAddress();
-		  // At this point, the user has already been prompted to either enter the URI
-		  // of a master to use or to start a master locally.
-		  NodeMainExecutor e = DefaultNodeMainExecutor.newDefault();
-
-		  NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(hostLocal,masterUri);
-		  nodeConfiguration.setMasterUri(masterUri);
-		  nodeConfiguration.setNodeName("android_sensor_message");
-
-		  talker = new AndroidSensorMessageNode(mSensorManager,sensors,usedFlag2);
-
-		  e.execute(talker, nodeConfiguration);
-
-
-		    	}catch(URISyntaxException e){
-		    		e.printStackTrace();
-		    	}
-		    }
-		}
-		    
-	
-	@Override
-	  protected void onHandleIntent(Intent intent) {
-		try {
-		      Thread.sleep(10000);
-		    } catch (InterruptedException e) {
-		      e.printStackTrace();
-		    }    
+	public void changeSensor(int num){
+		usedFlag2[num] *= -1;
+		main_node.changeSensor(num);
 		
 	}
 	
+	public void changeView(int num){
+		main_node.changeView(num);
+	}
 
-  /**
-   * Show a notification while this service is running.
-   */
-  private void showNotification() {
-	  
-		Intent i=new Intent(this, AndroidSensorMessage.class);
-		
-		i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|
-		Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		
-		PendingIntent pi=PendingIntent.getActivity(this, 0, i, 0);
-		
-  }
-  
-  @Override
-  public void onDestroy(){
-  	this.talker.onShutdown(null);
-  }
+    public void setNode(int[] flag,AndroidSensorMessageView view,TextView textViewX,TextView textViewY,TextView textViewZ) {
+		showNotification();
 
-	
+		for (int i = 0; i < 9; i++) {
+			usedFlag2[i] = flag[i];
+		}
+
+		// We use this bundle
+		Bundle b = mintent.getExtras();
+
+		if (this.main_node == null) {
+			try {
+				URI masterUri = new URI(b.getString("masterUri"));
+				String hostLocal = InetAddressFactory.newNonLoopback()
+						.getHostAddress();
+				// At this point, the user has already been prompted to either
+				// enter the URI
+				// of a master to use or to start a master locally.
+				NodeMainExecutor e = DefaultNodeMainExecutor.newDefault();
+
+				NodeConfiguration nodeConfiguration = NodeConfiguration
+						.newPublic(hostLocal, masterUri);
+				nodeConfiguration.setMasterUri(masterUri);
+				nodeConfiguration.setNodeName("android_sensor_message");
+
+				main_node = new AndroidSensorMessageNode(mSensorManager, sensors,view,textViewX,textViewY,textViewZ,usedFlag2);
+
+				e.execute(main_node, nodeConfiguration);
+
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Show a notification while this service is running.
+	 */
+	private void showNotification() {
+
+		Intent i = new Intent(this, AndroidSensorMessage.class);
+
+		i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+				| Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+		PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+	}
+
+	@Override
+	public void onDestroy() {
+		this.main_node.onShutdown(null);
+	}
+
 }
