b9286d08d5c1bd89cc5a98102f91066141b4c47f
kazuto <kazuto@jsk.t.u-tokyo.ac.jp>
2013-01-14 11:04:55 +0000

    add function to publish raw-sound data

---
+++ android_voice_message/src/org/ros/android/androidVoiceMessage/AndroidVoiceMessage.java
@@ -0,0 +1,209 @@
+package org.ros.android.androidVoiceMessage;
+
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import android.os.Bundle;
+import android.os.IBinder;
+
+import org.ros.android.MessageCallable;
+import org.ros.android.RosActivity;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+import org.ros.rosjava_tutorial_pubsub.Talker;
+
+//add
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import org.ros.node.topic.Publisher;
+
+import java.util.List;
+import jsk_gui_msgs.VoiceMessage;
+
+
+public class AndroidVoiceMessage extends RosActivity {
+
+	private URI masterUri;
+	private TextView textView;
+	private ImageView imageView;
+	private String packageName;
+	private int mode;
+
+	SpeechRecognizer speechRecignizer;
+	private SensorManager sensorManager;
+	private Intent intent;
+	private AndroidVoiceMessageService androidVoiceMessageService;
+	private AndroidVoiceMessageView androidVoiceMessageView;
+
+	ServiceConnection serviceConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			androidVoiceMessageService = ((AndroidVoiceMessageService.MyBinder) service)
+					.getService();
+			startService(intent);
+			imageView = (ImageView)findViewById(R.id.ImageView);
+			androidVoiceMessageService.setNode(speechRecignizer, textView,imageView, packageName);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			androidVoiceMessageService = null;
+		}
+
+	};
+
+	public AndroidVoiceMessage() {
+		super("AndroidVoiceMessage", "AndroidVoiceMessage");
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mode = 1;
+		textView = (TextView) findViewById(R.id.text);
+		imageView = (ImageView) findViewById(R.id.ImageView);
+		imageView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				androidVoiceMessageService.setStart();
+			}
+		});
+
+		// add
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		speechRecignizer = SpeechRecognizer.createSpeechRecognizer(this);
+		packageName = getPackageName();
+		androidVoiceMessageView = new AndroidVoiceMessageView(this);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (masterUri != null) {
+
+			intent = new Intent(getBaseContext(),
+					AndroidVoiceMessageService.class);
+			intent.putExtra("masterUri", masterUri.toString());
+			bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+		}
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == 0 && resultCode == RESULT_OK) {
+			try {
+				masterUri = new URI(data.getStringExtra("ROS_MASTER_URI"));
+			} catch (URISyntaxException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0,0,0,R.string.change_mode);
+		menu.add(0, 1, 1, R.string.app_about);
+		menu.add(0, 2, 1, R.string.str_exit);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		switch (item.getItemId()) {
+		case 0:
+			mode *= -1;
+			if(mode == 1){
+				setContentView(R.layout.main);
+				textView = (TextView) findViewById(R.id.text);
+				imageView = (ImageView) findViewById(R.id.ImageView);
+				imageView.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						androidVoiceMessageService.setStart();
+					}
+				});
+				androidVoiceMessageService.setView(textView,imageView);
+				androidVoiceMessageService.setRawModeStop();
+			}
+			else{
+				setContentView(R.layout.main2);
+				SurfaceView surfaceView = (SurfaceView) findViewById(R.id.SV);
+				androidVoiceMessageView.setView(surfaceView);
+				androidVoiceMessageService.setRawModeStart(androidVoiceMessageView);
+
+			}
+			break;
+		case 1:
+			openOptionsDialog();
+			break;
+		case 2:
+			exitOptionsDialog();
+			break;
+		}
+		return true;
+	}
+
+	private void openOptionsDialog() {
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.app_about)
+				.setMessage(R.string.app_about_message)
+				.setPositiveButton(R.string.str_ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(
+									DialogInterface dialoginterface, int i) {
+							}
+						}).show();
+	}
+
+	private void exitOptionsDialog() {
+		stopService(new Intent(this, AndroidVoiceMessageService.class));
+		finish();
+	}
+
+	@Override
+	protected void init(NodeMainExecutor nodeMainExecutor) {
+
+	}
+}
--- android_voice_message/src/org/ros/android/androidVoiceMessage/AndroidVoiceMessageNode.java
+++ android_voice_message/src/org/ros/android/androidVoiceMessage/AndroidVoiceMessageNode.java
@@ -1,8 +1,14 @@
-package org.ros.android.android_voice_message;
+package org.ros.android.androidVoiceMessage;
 
+import static org.jboss.netty.buffer.ChannelBuffers.LITTLE_ENDIAN;
+import static org.jboss.netty.buffer.ChannelBuffers.buffer;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jboss.netty.buffer.ChannelBuffer;
 import org.ros.namespace.GraphName;
 import org.ros.node.ConnectedNode;
 import org.ros.node.NodeMain;
@@ -15,6 +21,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Looper;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -22,14 +29,20 @@ import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 import jsk_gui_msgs.VoiceMessage;
 import android.view.View;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import audio_common_msgs.AudioData;
+import android.media.MediaRecorder;
 
 import org.ros.node.*;
 
 public class AndroidVoiceMessageNode implements NodeMain, SensorEventListener {
 
-	// add
+	// for text_message
 	private SensorManager mSensorManager; // センサーマネージャ
 	private List<Sensor> sensors;
 	private int[] flag;
@@ -41,6 +54,15 @@ public class AndroidVoiceMessageNode implements NodeMain, SensorEventListener {
 	private SpeechRecognizer sr;
 	private String package_name;
 	private TextView textView;
+	private ImageView imageView;
+	private AndroidVoiceMessageView androidVoiceMessageView;
+	// for raw_sound
+	private Publisher<audio_common_msgs.AudioData> audio_pub;
+	private audio_common_msgs.AudioData audio_msg;
+	final static int SAMPLING_RATE = 11025; // 8000,11025,16000,22050,44100
+	AudioRecord audioRec = null;
+	private int bufSize;
+	private int audioFlag = 0;
 
 	@Override
 	public GraphName getDefaultNodeName() {
@@ -48,39 +70,104 @@ public class AndroidVoiceMessageNode implements NodeMain, SensorEventListener {
 	}
 
 	public AndroidVoiceMessageNode(SensorManager manager, SpeechRecognizer sr,
-			TextView textView, String package_name) {
+			 String package_name) {
 		mSensorManager = manager;
 		this.sr = sr;
-		this.textView = textView;
 		sr.setRecognitionListener(new SpeechListener());
 		this.package_name = package_name;
+	}
+	
+	public void setView(TextView textView,ImageView imageView){
+		this.textView = textView;
+		this.imageView = imageView;
 	}
 
 	@Override
 	public void onStart(final ConnectedNode connectedNode) {
 
 		// センサー管理
-		/*List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+		/*
+		 * List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+		 * 
+		 * for (Sensor sensor : sensors) { int sensorType = sensor.getType();
+		 * switch (sensorType) { case Sensor.TYPE_MAGNETIC_FIELD:
+		 * mSensorManager.registerListener(this, sensor,
+		 * SensorManager.SENSOR_DELAY_NORMAL); } }
+		 */
 
-		for (Sensor sensor : sensors) {
-			int sensorType = sensor.getType();
-			switch (sensorType) {
-			case Sensor.TYPE_MAGNETIC_FIELD:
-				mSensorManager.registerListener(this, sensor,
-						SensorManager.SENSOR_DELAY_NORMAL);
-			}
-		}*/
+		// for raw_audio
+		bufSize = AudioRecord.getMinBufferSize(SAMPLING_RATE,
+				AudioFormat.CHANNEL_CONFIGURATION_MONO,
+				AudioFormat.ENCODING_PCM_16BIT) * 2;
 
 		try {
 			voice_pub = connectedNode.newPublisher("Tablet/voice",
 					"jsk_gui_msgs/VoiceMessage");
-			Log.v("voice", "publish start");
+			audio_pub = connectedNode.newPublisher("audio",
+					"audio_common_msgs/AudioData");
 			startFlag = 1;
 
 		} catch (Exception e) {
 
 		}
 
+		// AudioRecordの作成
+		audioRec = new AudioRecord(MediaRecorder.AudioSource.MIC,
+				SAMPLING_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+				AudioFormat.ENCODING_PCM_16BIT, bufSize);
+
+	}
+	
+	public void stopRawSound(){
+		audioFlag = 0;
+	}
+
+	public void publishRawSound(AndroidVoiceMessageView androidVoiceMessageView){
+		this.androidVoiceMessageView = androidVoiceMessageView;
+		audioFlag = 1;
+		audioRec.startRecording();
+		// Record
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+				try {
+
+					byte buf[] = new byte[bufSize];
+
+					while (audioFlag == 1) {
+
+						int returnSize = audioRec.read(buf, 0, buf.length);
+
+						audio_msg = audio_pub.newMessage();
+						ChannelBuffer heapBuffer = buffer(LITTLE_ENDIAN,
+								bufSize);
+						heapBuffer.writeBytes(buf);
+						audio_msg.setData(heapBuffer);
+						audio_pub.publish(audio_msg);
+						
+						double volume = 0;
+						for(int i = 0; i < buf.length;i++){
+							volume += Math.abs(buf[i]);
+						}
+						volume/= buf.length;
+						addValue((float)volume);
+						Log.v("buf-size",""+volume);
+						 
+					}
+					Log.v("AudioRecord", "stop");
+					audioRec.stop();
+
+				} catch (Exception e) {
+
+				}
+			}
+		}).start();
+
+	}
+	
+	private void addValue(float volume){
+		androidVoiceMessageView.addValues(volume);
 	}
 
 	@Override
@@ -139,6 +226,7 @@ public class AndroidVoiceMessageNode implements NodeMain, SensorEventListener {
 					package_name);
 			Log.v("voice", "button pushed!");
 			sr.startListening(intent);
+			imageView.setImageResource(R.drawable.mike2);
 
 		}
 
@@ -149,40 +237,39 @@ public class AndroidVoiceMessageNode implements NodeMain, SensorEventListener {
 
 		@Override
 		public void onBeginningOfSpeech() {
-			Log.v("voice", "ready!");
+			imageView.setImageResource(R.drawable.mike3);
 		}
 
 		@Override
 		public void onBufferReceived(byte[] buffer) {
 			// TODO Auto-generated method stub
-			Log.v("voice", "buffer!");
 			receiveFlag++;
 		}
 
 		@Override
 		public void onEndOfSpeech() {
-			Log.v("voice", "end!");
+			imageView.setImageResource(R.drawable.mike);
 			busyFlag = 2;
 
 			// TODO Auto-generated method stub
-			/*try{ Thread.sleep(1000);
-			 sr.stopListening();
-			   Thread.sleep(1000);
-		  
-		  } catch(Exception e){ }
-			
-			Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-			intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-					RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-			intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
-					package_name);
-			Log.v("voice", "restart!");
-			sr.startListening(intent);
-*/
+			/*
+			 * try{ Thread.sleep(1000); sr.stopListening(); Thread.sleep(1000);
+			 * 
+			 * } catch(Exception e){ }
+			 * 
+			 * Intent intent = new
+			 * Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+			 * intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+			 * RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+			 * intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+			 * package_name); Log.v("voice", "restart!");
+			 * sr.startListening(intent);
+			 */
 		}
 
 		@Override
 		public void onError(int error) {
+			imageView.setImageResource(R.drawable.mike);
 
 			switch (error) {
 			case SpeechRecognizer.ERROR_AUDIO:
@@ -223,15 +310,13 @@ public class AndroidVoiceMessageNode implements NodeMain, SensorEventListener {
 				break;
 			default:
 			}
-			
-			
-			/*  try{ Thread.sleep(1000);
-				 sr.stopListening();
-				   Thread.sleep(1000);
-			  
-			  } catch(Exception e){ }
-*/
-				
+
+			/*
+			 * try{ Thread.sleep(1000); sr.stopListening(); Thread.sleep(1000);
+			 * 
+			 * } catch(Exception e){ }
+			 */
+
 			// TODO Auto-generated method stub
 
 		}
