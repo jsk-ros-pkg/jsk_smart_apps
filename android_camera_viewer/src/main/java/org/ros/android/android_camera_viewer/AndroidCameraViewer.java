3e2176f96b7e417314021b12e5424e7499a291a7
KazutoMurase <kattyokaccho3616@gmail.com>
2014-03-21 21:19:46 +0900

    catkinized android_camera_viewer

---
+++ android_camera_viewer/src/main/java/org/ros/android/android_camera_viewer/AndroidCameraViewer.java
@@ -0,0 +1,15 @@
+package org.ros.android.android_camera_viewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidCameraViewer extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
