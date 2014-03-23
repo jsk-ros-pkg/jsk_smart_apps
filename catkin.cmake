672e291ea7f10f17353a267d125c6ed014aaefcc
KazutoMurase <kattyokaccho3616@gmail.com>
2014-03-23 17:01:34 +0900

    delete extra files

--- catkin.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 2.8.3)
-project(jsk_smart_apps)
-
-find_package(catkin REQUIRED rosjava_build_tools)
-# Set the gradle targets you want catkin's make to run by default
-catkin_android_setup(assembleRelease uploadArchives)
-catkin_package()
-
-
